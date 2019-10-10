c8dc6567ac940641aff55fbdb640058dc1872c28
tomitri <tomitri@yahoo.fr>
2019-10-10 23:36:10 +0200

    Update StepDefinitions.java

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,1 +1,19 @@
-
+public class StepDefinitions implements En {
+ 
+    private Person personToTest = new Person();
+ 
+    private PersonDAO personBDD;
+ 
+    public StepDefinitions() {
+ 
+        Given("^ Nouvelle Adresse $", () -> {
+            personToTest.SetAdresse();
+        });
+ 
+        When("^j'enregistre la personne avec la nouvelle adresse $", () -> {
+            personBDD.update(personToTest);
+        });
+ 
+    }
+ 
+}
